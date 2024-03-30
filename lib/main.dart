11bdc5881b1060fa8557fd42cb67af219968e7cb
youxianda <youxianda@meituan.com>
2024-03-30 12:51:28 +0800

    配置iOS编译

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,12 @@ import 'services/app_init_service.dart';
 import 'utils/page_path_util.dart';
 
 // flutter build apk --split-per-abi
+
+// Xcode编译问题
+// delete Podfile
+// run 【flutter clean】, 【flutter pub get】 in your Flutter program, then Podfile will be generated
+// In new Podfile, adjust platform to 14.0 and Uncomment that line
+// run 【pod install】 in your iOS program
 void main() async {
   // 全局初始化
   await AppInitService.init();
